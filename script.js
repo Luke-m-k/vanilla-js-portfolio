30b5790f2e2a0b69842be3151cc0c514b3903a35
luke <kamau.luke.m@gmail.com>
2025-08-21 19:27:14 +0300

    feat(ui): add all the sections in a mobile friendly way

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+const yearEl = document.getElementById('current-year');
+yearEl.textContent = new Date().getFullYear();
+
 function toggleMenu() {
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
